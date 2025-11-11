17202c2409f79ac31b1ba287ccee1a1b7c5fa182
Andrea Silvi <andrea.silvi94@gmail.com>
2025-11-11 17:54:52 +0100

    fixed jest

---
+++ packages/core/jest.setup.js
@@ -0,0 +1,6 @@
+// Polyfill for TextDecoder/TextEncoder in Jest environment
+if (typeof globalThis.TextDecoder === 'undefined') {
+  const { TextDecoder, TextEncoder } = require('util');
+  globalThis.TextDecoder = TextDecoder;
+  globalThis.TextEncoder = TextEncoder;
+}
